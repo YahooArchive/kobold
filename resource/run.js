52194be7b37e1f2f25ec40d0ce1c09faab18f8fd
Marcel Erz <erz@yahoo-inc.com>
2014-12-10 14:21:04 -0800

    Bugfix for Windows paths (#1)

--- resource/run.js
+++ resource/run.js
@@ -1,5 +1,5 @@
 // Copyright 2014, Yahoo! Inc.
 // Copyrights licensed under the Mit License. See the accompanying LICENSE file for terms.
 
-var runner = require('./../index');
+var runner = require('../index');
 runner(global.koboldOptions);
